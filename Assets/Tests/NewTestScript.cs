7243828f27ed95a6559e2d123ba538a3a2f66fba
Christopher West (Arynoyn) <Arynoyn@gmail.com>
2021-05-14 10:00:11 -0500

    Add simple Unit Test

---
+++ Assets/Tests/NewTestScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class NewTestScript
+{
+    // A Test behaves as an ordinary method
+    [Test]
+    public void NewTestScriptSimplePasses()
+    {
+        GameObject sut = new GameObject("MyGameObject");
+        Assert.AreEqual("MyGameObject", sut.name);
+    }
+
+    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+    // `yield return null;` to skip a frame.
+    [UnityTest]
+    public IEnumerator NewTestScriptWithEnumeratorPasses()
+    {
+        // Use the Assert class to test conditions.
+        // Use yield to skip a frame.
+        yield return null;
+    }
+}
